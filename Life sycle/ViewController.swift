a7b887f0f6a08c5137446402ece4300be2c0ecf0
amrfoda <amrfoda13@gmail.com>
2021-01-03 19:36:19 +0200

    Initial Commit

---
+++ Life sycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life sycle
+//
+//  Created by amr  on 1/3/21.
+//  Copyright © 2021 amr 25. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
